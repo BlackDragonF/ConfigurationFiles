bb863e68ba98b6828f2add77e1aa6cb3273b2856
BlackDragonF <obsidiandragon2016@gmail.com>
2018-01-12 13:44:09 +0800

    update hyper.js to canary-2.0.0

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,5 @@
 # set greeting
 set fish_greeting '/* may the data wind through the vally */' 
-# switch to vi mode
-set -g fish_key_bindings fish_vi_key_bindings
 # set EDITOR
 set -x EDITOR vim
 # set powerline
--- hyper.js
+++ hyper.js
@@ -4,73 +4,80 @@
 
 module.exports = {
   config: {
-    // Choose either "stable" for receiving highly polished,
-    // or "canary" for less polished but more frequent updates
+    // choose either `'stable'` for receiving highly polished,
+    // or `'canary'` for less polished but more frequent updates
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 18,
+    fontSize: 16,
 
     // font family with optional fallbacks
-    fontFamily: '"Fira Code Retina", "Source Code Pro Regular", "WenQuanYi Micro Hei Mono", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: '"Source Code Pro Regular", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-    cursorColor: 'rgba(255,255,255,0.8)',
+    cursorColor: 'rgba(44,133,247,0.8)',
 
-    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+    // terminal text color under BLOCK cursor
+    cursorAccentColor: 'rgb(205,210,233)',
+
+    // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
     cursorShape: 'BLOCK',
 
-    // set to true for blinking cursor
+    // set to `true` (without backticks and without quotes) for blinking cursor
     cursorBlink: true,
 
     // color of the text
-    foregroundColor: '#fff',
+    foregroundColor: 'rgb(205,210,233)',
 
     // terminal background color
-    backgroundColor: '#000',
+    // opacity is only supported on macOS
+    backgroundColor: 'rgb(41,51,64)',
+
+    // terminal selection color
+    selectionColor: 'rgba(145,152,162,0.3)',
 
     // border color (window, tabs)
-    borderColor: '#333',
+    borderColor: 'rgb(50,62,77)',
 
-    // custom css to embed in the main window
+    // custom CSS to embed in the main window
     css: '',
 
-    // custom css to embed in the terminal window
+    // custom CSS to embed in the terminal window
     termCSS: '',
 
     // set to `true` (without backticks and without quotes) if you're using a
     // Linux setup that doesn't show native menus
-    // default: `false` on Linux, `true` on Windows (ignored on macOS)
-    showHamburgerMenu: true,
+    // default: `false` on Linux, `true` on Windows, ignored on macOS
+    showHamburgerMenu: '',
 
-    // set to `false` if you want to hide the minimize, maximize and close buttons
+    // set to `false` (without backticks and without quotes) if you want to hide the minimize, maximize and close buttons
     // additionally, set to `'left'` if you want them on the left, like in Ubuntu
-    // default: `true` on windows and Linux (ignored on macOS)
+    // default: `true` (without backticks and without quotes) on Windows and Linux, ignored on macOS
     showWindowControls: '',
 
-    // custom padding (css format, i.e.: `top right bottom left`)
+    // custom padding (CSS format, i.e.: `top right bottom left`)
     padding: '12px 14px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
     // an array here instead of a color map object
     colors: {
-      black: '#000000',
-      red: '#ff0000',
-      green: '#33ff00',
-      yellow: '#ffff00',
-      blue: '#0066ff',
-      magenta: '#cc00ff',
-      cyan: '#00ffff',
-      white: '#d0d0d0',
-      lightBlack: '#808080',
-      lightRed: '#ff0000',
-      lightGreen: '#33ff00',
-      lightYellow: '#ffff00',
-      lightBlue: '#0066ff',
-      lightMagenta: '#cc00ff',
-      lightCyan: '#00ffff',
-      lightWhite: '#ffffff',
+      black: '#293340',
+      red: '#E17E85',
+      green: '#61BA86',
+      yellow: '#FFEC8E',
+      blue: '#4CB2FF',
+      magenta: '#BE86E3',
+      cyan: '#2DCED0',
+      white: '#CDD2E9',
+      lightBlack: '#546386',
+      lightRed: '#E17E85',
+      lightGreen: '#61BA86',
+      lightYellow: '#FFB68E',
+      lightBlue: '#4CB2FF',
+      lightMagenta: '#BE86E3',
+      lightCyan: '#2DCED0',
+      lightWhite: '#CDD2D9',
     },
 
     // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
@@ -83,25 +90,25 @@ module.exports = {
     // Bash on Windows
     // - Example: `C:\\Windows\\System32\\bash.exe`
     //
-    // Powershell on Windows
+    // PowerShell on Windows
     // - Example: `C:\\WINDOWS\\System32\\WindowsPowerShell\\v1.0\\powershell.exe`
-    shell: 'fish',
+    shell: '/usr/bin/fish',
 
-    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
-    // by default ['--login'] will be used
+    // for setting shell arguments (i.e. for using interactive shellArgs: `['-i']`)
+    // by default `['--login']` will be used
     shellArgs: ['--login'],
 
     // for environment variables
     env: {},
 
-    // set to false for no bell
+    // set to `false` for no bell
     bell: 'SOUND',
 
-    // if true, selected text will automatically be copied to the clipboard
+    // if `true` (without backticks and without quotes), selected text will automatically be copied to the clipboard
     copyOnSelect: false,
 
-    // if true, on right click selected text will be copied or pasted if no
-    // selection is present (true by default on Windows)
+    // if `true` (without backticks and without quotes), on right click selected text will be copied or pasted if no
+    // selection is present (`true` by default on Windows and disables the context menu feature)
     // quickEdit: true,
 
     // URL to custom bell
@@ -116,7 +123,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyper-chesterish', 'hyper-ligatures'],
+  plugins: [],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
