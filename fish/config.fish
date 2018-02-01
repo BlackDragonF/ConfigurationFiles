eeb839b0a6ff8ee512ed51c224c48791f5a3a70d
BlackDragonF <obsidiandragon2016@gmail.com>
2018-02-02 00:44:01 +0800

    add module files for jos

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,6 @@ set fish_function_path $fish_function_path "/usr/lib/python3.6/site-packages/pow
 powerline-setup
 # set intel vtune amplifier
 test -e {$HOME}/.amplxe-vars.fish ; and source {$HOME}/.amplxe-vars.fish
+# set environment modules
+test -e /etc/modules/init/fish; and source /etc/modules/init/fish
+test -e /etc/modules/init/fish_completion; and source /etc/modules/init/fish_completion
